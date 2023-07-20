412c85b08a02c06ab4246c7aa96a78c8bde57b03
chandn33 <misschandni_patel003@hotmail.co.uk>
2023-07-20 22:04:58 +0100

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-hello
-      </header>
+      <div className="container">
+    <h1>Dictionary</h1>
+    <main>
+      <Dictionary />
+    </main>
+
+
+    <footer>Open-source on <a href="https://github.com/chandn33/dictionary-project" target="_blank" rel="noopener noreferrer">GitHub</a> | Project designed by Chandni Patel🌻</footer>
+    </div>
     </div>
   );
-}
+  }
